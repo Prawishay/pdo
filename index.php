8d7c2cc4496f1ba0d3430ef97a532bd98e383730
Prawishay <133200622+Prawishay@users.noreply.github.com>
2023-05-25 11:48:50 +0200

    Add files via upload
    week 2 opdracht

--- index.php
+++ index.php
@@ -1,25 +1,34 @@
-<?php
-$toei = 12;
-$prawi = 10;
-echo $toei + $prawi;
+<!DOCTYPE html>
+<html lang="en">
+<head>
+    <meta charset="UTF-8">
+    <meta http-equiv="X-UA-Compatible" content="IE=edge">
+    <meta name="viewport" content="width=, initial-scale=1.0">
+    <title>Document</title>
+</head>
+<body>
+    <?php
+        $host = 'localhost:3307';
+    $db   = 'test';
+    $user = 'root';
+    $pass = '';
+    $charset = 'utf8mb4';
 
+    $dsn = "mysql:host=$host;dbname=$db;charset=$charset";
+    $options = [
+    PDO::ATTR_ERRMODE            => PDO::ERRMODE_EXCEPTION,
+    PDO::ATTR_DEFAULT_FETCH_MODE => PDO::FETCH_ASSOC,
+    PDO::ATTR_EMULATE_PREPARES   => false,
+    ];
 
-$t = 12;
-$p = 10;
-echo $t * $p;
+    try 
+    { $pdo = new PDO($dsn, $user, $pass, $options); 
+    echo 'connnectie met database';
+    } 
 
+    catch (\PDOException $e) 
 
-$t = "toei";
-echo "welkom" . $t . "test";
-
-
-$mijnNaam="Carl";
-$jouwNaam="Camel";
-echo $mijnNaam . " zit bij " . $jouwNaam . " in de klas!"
-
-
-
-
-
-
-?>
+    { throw new \PDOException($e->getMessage(), (int)$e->getCode());}
+    ?>
+</body>
+</html>
